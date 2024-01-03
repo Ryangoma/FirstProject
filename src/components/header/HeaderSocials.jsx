c00f95267239e193ad9fd055c61e3e8a102d2796
Ryangoma <150627753+Ryangoma@users.noreply.github.com>
2024-01-02 23:33:38 -0500

    second commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
