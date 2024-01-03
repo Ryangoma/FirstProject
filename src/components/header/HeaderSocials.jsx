b3cfcacd14b75761eef6413d8e159367714adf6c
Ryangoma <150627753+Ryangoma@users.noreply.github.com>
2024-01-02 23:11:05 -0500

    First Commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ import { FaInstagram } from "react-icons/fa";
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/chrisrtabaro" target='_blank'><FaLinkedin/></a>
+        <a href="https://www.linkedin.com/in/chrisrtabaro" target="_blank"><FaLinkedin/></a>
         <a href="https://instagram.com/9goma" target='_blank'><FaInstagram/></a>
         <a href="https://github.com/ryangoma" target='_blank'><FaGithub/></a>
     </div>
